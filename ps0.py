9bd9bfb8d73f38376290c04b3717302d1a619569
Jeffreyb723 <34499736+Jeffreyb723@users.noreply.github.com>
2021-03-18 21:15:58 -0400

    Update ps0.py

--- ps0.py
+++ ps0.py
@@ -1,3 +1,9 @@
+"""
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y”
+3. Prints out number “x”, raised to the power “y”.
+4. Prints out the log (base 2) of “x”.
+"""
 import numpy
 x = float(input("Enter a number for x: "))
 y = float(input("Enter a number for y: "))
